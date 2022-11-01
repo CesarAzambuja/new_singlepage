5ec86e2452819a47bcbf5fbdf3c0771a61e1de64
CesarAzambuja <caaso.eng@gmail.com>
2022-11-01 15:38:34 -0300

    Ajuste responsividade

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
 document.querySelector(".hamburguer").addEventListener("click", () => {
     document.querySelector(".container").classList.toggle("show-menu")
 })
+
+document.querySelector(".menu").addEventListener("click", () => {
+    document.querySelector(".container").classList.toggle("show-menu")
+})
+
+
+
+function typeWrite(elemento){
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    textoArray.forEach((letra, i) => {
+        
+        setTimeout(function(){
+            elemento.innerHTML += letra
+        }, 95 * (i+20))
+    })
+
+    console.log(elemento)
+}
+
+const titulo = document.querySelector('.title')
+typeWrite(titulo);
+
